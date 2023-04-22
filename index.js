eb3b33422dcc5b57bb60bcb1abbc3dc99efd5780
gipsy <ter4444@gmail.com>
2023-04-22 14:49:58 +0300

    added updatePost,deletePost,likePost logics

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://wellcode:CaIm8CqcbINrO8rS@clustermemories.8ysxql9.mongodb.net/?retryWrites=true&w=majority'
+const CONNECTION_URL = 'mongodb+srv://wellcode:08WIccIDEmOb9Gpf@clustermemories.8ysxql9.mongodb.net/?retryWrites=true&w=majority'
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL)
