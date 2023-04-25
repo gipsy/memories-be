398dc229857ccd5758c86b458e268871eec0b5b4
gipsy <ter4444@gmail.com>
2023-04-25 16:48:09 +0300

    accomplished auth logic

--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,4 @@
-import bcrypt from 'bcrypt.js';
+import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 
 import User from '../models/user.js';
@@ -18,7 +18,7 @@ export const signin = async (req, res) => {
 
     const token = jwt.sign({ email: existingUser.email, id: existingUser._id }, 'test', { expiresIn: "1h" });
 
-    res.status(200).json({ result: existingUser, token });
+    res.status(200).json({ userInfo: existingUser, token });
   } catch (error) {
     res.status(500).json({ message: 'Something went wrong.' });
   }
@@ -36,11 +36,11 @@ export const signup = async (req, res) => {
 
     const hashedPassword = await bcrypt.hash(password, 12);
 
-    const result = await User.create({ email, password: hashedPassword, name: `${firstName} ${lastName}` });
+    const userInfo = await User.create({ email, password: hashedPassword, name: `${firstName} ${lastName}` });
 
-    const token = jwt.sign({ email: result.email, id: result._id }, 'test', { expiresIn: "1h" });
+    const token = jwt.sign({ email: userInfo.email, id: userInfo._id }, 'test', { expiresIn: "1h" });
 
-    res.status(200).json({ result, token });
+    res.status(200).json({ userInfo, token });
   } catch (error) {
     res.status(500).json({ message: 'Something went wrong.' });
   }
