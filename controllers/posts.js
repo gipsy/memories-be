c9060711b557b20bcc6964b226b23823539ed320
gipsy <ter4444@gmail.com>
2023-04-26 20:58:41 +0300

    migrated to google oAuth2 v3

--- controllers/posts.js
+++ controllers/posts.js
@@ -11,27 +11,39 @@ export const getPosts = async (req, res) => {
   }
 }
 
+export const getPost = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const post = await PostMessage.findById(id);
+
+    res.status(200).json(post);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+}
+
 export const createPost = async (req, res) => {
   const post = req.body;
 
-  const newPost = new PostMessage(post);
+  const newPostMessage = new PostMessage({ ...post, creator: req.userId, createdAt: new Date().toISOString() });
 
   try {
-    await newPost.save();
+    await newPostMessage.save();
 
-    res.status(201).json(newPost);
+    res.status(201).json(newPostMessage);
   } catch (error) {
     res.status(409).json({ message: error.message })
   }
 }
 
 export const updatePost = async (req, res) => {
-  const { id: _id } = req.params;
+  const { id } = req.params;
   const post = req.body;
 
-  if (!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post with that id');
+  if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with id: ${id}`);
 
-  const updatedPost = await PostMessage.findByIdAndUpdate(_id, { ...post, _id }, { new: true });
+  const updatedPost = await PostMessage.findByIdAndUpdate(id, { ...post, _id: id }, { new: true });
 
   res.json(updatedPost);
 }
--- controllers/user.js
+++ controllers/user.js
@@ -1,24 +1,39 @@
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
+import { OAuth2Client } from 'google-auth-library';
 
 import User from '../models/user.js';
-import mongoose from 'mongoose';
 
 export const signin = async (req, res) => {
-  const { email, password } = req.body;
+  const { email, password, googleCode } = req.body;
+
+  const oAuth2Client = new OAuth2Client(
+    process.env.GOOGLE_CLIENT_ID,
+    process.env.GOOGLE_CLIENT_SECRET,
+    'postmessage',
+  );
 
   try {
-    const existingUser = await User.findOne({ email });
+    if (!!googleCode) {
+      const { tokens: { refresh_token, id_token, expiry_date } } = await oAuth2Client.getToken(googleCode); // exchange
+      // code for tokens
 
-    if (!existingUser) return res.status(404).json({ message: "User doesn't exist." });
+      const userInfo = jwt.decode(id_token);
 
-    const isPasswordCorrect = await bcrypt.compare(password, existingUser.password);
+      res.status(200).json({ userInfo, token: id_token });
+    } else {
+      const existingUser = await User.findOne({ email });
 
-    if (!isPasswordCorrect) return res.status(400).json({ message: "Invalid credentials." });
+      if (!existingUser) return res.status(404).json({ message: "User doesn't exist." });
 
-    const token = jwt.sign({ email: existingUser.email, id: existingUser._id }, 'test', { expiresIn: "1h" });
+      const isPasswordCorrect = await bcrypt.compare(password, existingUser.password);
 
-    res.status(200).json({ userInfo: existingUser, token });
+      if (!isPasswordCorrect) return res.status(400).json({ message: "Invalid credentials." });
+
+      const token = jwt.sign({ email: existingUser.email, id: existingUser._id }, 'test', { expiresIn: "1h" });
+
+      res.status(200).json({ userInfo: existingUser, token });
+    }
   } catch (error) {
     res.status(500).json({ message: 'Something went wrong.' });
   }
