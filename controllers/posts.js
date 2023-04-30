f1bc53c75481f1994e51bdfe15908659934501ec
gipsy <ter4444@gmail.com>
2023-04-30 19:27:30 +0300

    accomlished post comments logic

--- controllers/posts.js
+++ controllers/posts.js
@@ -2,10 +2,15 @@ import PostMessage from '../models/postMessage.js';
 import mongoose from "mongoose";
 
 export const getPosts = async (req, res) => {
+  const { page } = req.query;
   try {
-    const postMessages = await PostMessage.find();
+    const LIMIT = 8;
+    const startIndex = (Number(page) - 1) * LIMIT; // get the starting index of every page
+    const total = await PostMessage.countDocuments({});
 
-    res.status(200).json(postMessages);
+    const posts = await PostMessage.find().sort({ _id: -1 }).limit(LIMIT).skip(startIndex);
+
+    res.status(200).json({ data: posts, currentPage: Number(page), numberOfPages: Math.ceil(total / LIMIT) });
   } catch (error) {
     res.status(404).json({ message: error.message });
   }
@@ -92,6 +97,19 @@ export const likePost = async (req, res) => {
   const updatedPost = await PostMessage.findByIdAndUpdate(
     id, post, { new: true }
   );
+
+  res.json(updatedPost);
+}
+
+export const commentPost = async (req, res) => {
+  const { id } = req.params;
+  const { value } = req.body;
+
+  const post = await PostMessage.findById(id);
+
+  post.comments.push(value);
+
+  const updatedPost = await PostMessage.findByIdAndUpdate(id, post, { new: true });
 
   res.json(updatedPost);
 }
